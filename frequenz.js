df8c51b9e7a6afee69758ebb1835d7447a0f4544
Zyntos <zyrielsleepwalker@gmail.com>
2017-09-26 18:49:27 +0200

    Update frequenz.js
    awf

--- frequenz.js
+++ frequenz.js
@@ -52,4 +52,4 @@ var button1 = document.getElementById("button1");
 button1.addEventListener("click", function(e){
 	document.getElementById("inputField").innerHTML = "hallo";
 	document.getElementById("div1").innerHTML = allFrequencies[document.getElementById("inputField").innerHTML];
-}	
+});	
